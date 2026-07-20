3b5b02df9839c2029779e3a581adc0c3a7446e75
Roshan Kumar Singh <94280658+R0shanSingh@users.noreply.github.com>
2026-07-20 15:26:19 +0530

    Create Leetcode_1283.cpp

---
+++ Session_2/Leetcode_1283.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    int smallestDivisor(vector<int>& nums, int threshold) {
+        int low=1;
+        int high = *max_element(nums.begin(),nums.end());
+        if(threshold == nums.size()) return high;
+        int output;
+        while(low<=high){
+            int mid = low + (high-low)/2;
+            int sum=0;
+            for(int el:nums){
+                sum += (el+mid-1)/mid;
+            }
+            if(sum<=threshold){
+                output=mid;
+                high = mid-1;
+            }
+            else low = mid+1;
+        }
+        return output;
+    }
+};
